ffb1ebfeb20c1a8f3db88470a9ea3feac5d62e96
richar <richarfang@xiaoe-tech.com>
2020-01-16 10:52:10 +0800

    组件库

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-const hello = require('./helloworld')
+export default function add(a, b) {
+    let i = a.length - 1;
+    let j = b.length - 1;
+
+    let carry = 0;
+    let ret = '';
+    while(i >= 0 || j >= 0) {
+        let x = 0;
+        let y = 0;
+        let sum;
+
+        if (i >= 0) {
+            x = a[i] - 0;
+            i --;
+        }
+
+        if (j >= 0) {
+            y = b[j] - 0;
+            j --;
+        }
+
+        sum = x + y + carry;
+
+        if (sum >= 10) {
+            carry = 1;
+            ret += sum - 10;
+        } else {
+            ret += sum;
+        }
+    }
+
+    if (carry) {
+        ret = carry + ret;
+    }
+    return ret;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,25 @@
 const path = require('path');
-const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: './src/index.js',
-        search: './src/search.js'
+        'large-number': './src/index.js',
+        'large-number.min': './src/index.js'
     },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name].js',
+        library: 'largeNumber',
+        libraryTarget: 'umd',
+        libraryExport: 'default'
     },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader'
-            }, {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            }, {
-                test: /\.less$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader'
-                ]
-            }, {
-                test: /\.(jpg|jpeg|png|gif)$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 102400
-                    }
-                }]
-            }
+    mode: 'none',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                include: /\.min\.js$/
+            })
         ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        contentBase: './dist',
-        hot: true
     }
 }
